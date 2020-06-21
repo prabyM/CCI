3e1f12820486631c05097f20593c1aca34467938
Prabhakar <mishra_prabhakar@outlook.com>
2020-06-21 16:02:04 +0530

    Updated phne format

--- Regex/count_of_most_frequently_occuring_phone_number.py
+++ Regex/count_of_most_frequently_occuring_phone_number.py
@@ -1,6 +1,8 @@
 import re
-st = "My name is Prabhakar and my contact number is 7903874985. I can also be contacted on 8751916801 or 8705621682. 7903874985 should be my primary contact."
-result = re.findall(r'\d\d\d\d\d\d\d\d\d\d',st)
+# The patern of number to be searched is like 878-898-7656
+
+st = "My name is Prabhakar and my contact number is 790-387-4985. I can also be contacted on 875-191-6801 or 870-562-1682. 790-387-4985 should be my primary contact."
+result = re.findall(r'[1-9]\d{2}-\d{3}-\d{4}',st)
 count_dict = {}
 for retrieved_numbers in result:
     if retrieved_numbers not in count_dict:
@@ -9,5 +11,4 @@ for retrieved_numbers in result:
         count_dict[retrieved_numbers] = count_dict[retrieved_numbers] + 1
 
 most_freq_number = max(count_dict, key=count_dict.get)
-print("Most frequetly seen number is: %s" %most_freq_number)
-
+print(most_freq_number)
